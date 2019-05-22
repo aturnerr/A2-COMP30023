19014c3e8514244fe3fdd5939e971bb42db7696c
Adam <a.turner11@student.unimelb.edu.au>
2019-05-22 12:59:21 +1000

    new wordlists, mutate case with depth

--- word filter/word_list_filter.c
+++ word filter/word_list_filter.c
@@ -7,12 +7,13 @@
 int main(int argc, char** argv){
     FILE* read_fp = fopen(argv[1], "r");
     FILE* write_fp = fopen(argv[2], "a");
-    int n = atoi(argv[3]);
+    int upper = atoi(argv[3]);
+    int lower = atoi(argv[4]);
 
     char word[MAX_WORD_LEN+1];
 
     while (fscanf(read_fp, "%s", word) == 1){
-        if (strlen(word) == n){
+        if ((strlen(word) <= upper) && (strlen(word) >= lower)){
             fprintf(write_fp, "%s\n", word);
         }
     }
